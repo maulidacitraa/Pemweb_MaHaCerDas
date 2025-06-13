4b2706e3891673dd4bb260a7651988a9b389cf52
maulidacitraa <maulida.ellis@gmail.com>
2025-06-14 00:42:46 +0800

    fix

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,11 @@
 <?php
-// Konfigurasi koneksi
-$host = "localhost";      // Biasanya 'localhost' di XAMPP/Laragon
-$user = "root";           // Username default MySQL
-$pass = "";               // Kosong jika pakai XAMPP default
-$db   = "mahacerdas";     // Nama database kamu
+$host = "localhost";      
+$user = "root";           
+$pass = "";               
+$db   = "mahacerdas";     
 
-// Buat koneksi
 $conn = mysqli_connect($host, $user, $pass, $db);
 
-// Cek koneksi
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
